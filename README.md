e664b194f6ec73580d23c00d443b3668d26d345a
akshay <akshaymoreJL04@gmail.com>
2020-02-18 22:59:34 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Immortals
+
+Developed with Unreal Engine 4
---
+++ Source/Immortals/ImmortalsGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "ImmortalsGameMode.h"
+#include "ImmortalsCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AImmortalsGameMode::AImmortalsGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
